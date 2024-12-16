7a6d333379e0322732756886fc44dbbc65b6d299
matve <matvey.karasev.00@gmail.com>
2024-12-16 12:00:30 +0300

    HW16.3 was completed

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //todo ФИО
+        final String VALID= "([А-я-]+\\s){2}([А-я]+)";
+        String fio = new Scanner(System.in).nextLine();
+        if (fio.matches(VALID)) {
+            String[] fioArr = fio.split("\\s");
+            System.out.println("Фамилия: " + fioArr[0]);
+            System.out.println("Имя: " + fioArr[1]);
+            System.out.println("Отчество: " + fioArr[2]);
+        }
+        else
+            System.out.println("Введенная строка не является ФИО");
+
+
     }
 }
