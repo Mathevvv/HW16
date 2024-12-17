13f2b37d0b47ffa380e9f00ca8d1009c6845a95d
matve <matvey.karasev.00@gmail.com>
2024-12-17 10:46:38 +0300

    HW16.4 was completed

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //todo Numbers
+        System.out.println("Введите номер телефона");
+        String num  = new Scanner(System.in).nextLine();
+        num = num.replaceAll("\\D", "");
+        boolean check = true;
+
+        if ((num.length()!=10)&&(num.charAt(0)!='7'))
+            check=false;
+
+        if (check) System.out.println(num);
+        else System.out.println("сообщение о неверном вводе");
     }
 }
